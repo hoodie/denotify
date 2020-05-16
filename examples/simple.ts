7c70aabd792f9ef9f7e59f58dbb00c187504fafd
Hendrik Sollich <hendrik@hoodie.de>
2020-05-16 15:52:12 +0200

    feat: show minimal notifications

--- examples/simple.ts
+++ examples/simple.ts
@@ -3,5 +3,5 @@ import { Notification } from "../mod.ts";
 const notification = Notification.new()
   .summary("test 🤔🤞")
   .appname("simple example")
-  .icon("success")
+  .icon("firefox")
   .show();
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-use serde::{Serialize, Deserialize};
 use deno_core::plugin_api::{Buf, Interface, Op, ZeroCopyBuf};
+use serde::{Deserialize, Serialize};
 
 #[cfg(all(unix, not(target_os = "macos")))]
 use std::collections::HashSet;
 
-
 #[no_mangle]
-pub fn deno_plugin_init(interface: &mut dyn Interface){
+pub fn deno_plugin_init(interface: &mut dyn Interface) {
     interface.register_op("show", show);
-} 
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Notification {
@@ -23,10 +22,10 @@ pub struct Notification {
 
     /// Multiple lines possible, may support simple markup,
     /// check out `get_capabilities()` -> `body-markup` and `body-hyperlinks`.
-    pub body:    Option<String>,
+    pub body: Option<String>,
 
     /// Use a file:// URI or a name in an icon theme, must be compliant freedesktop.org.
-    pub icon:    Option<String>,
+    pub icon: Option<String>,
 
     // /// Check out `Hint`
     // #[cfg(all(unix, not(target_os = "macos")))]
@@ -35,8 +34,35 @@ pub struct Notification {
     pub actions: Option<Vec<String>>,
     pub sound_name: Option<String>, // macos&windows
     pub path_to_image: Option<String>, // windows
-    // /// Lifetime of the Notification in ms. Often not respected by server, sorry.
-    // pub timeout: Timeout, // both gnome and galago want allow for -1
+                                    // /// Lifetime of the Notification in ms. Often not respected by server, sorry.
+                                    // pub timeout: Timeout, // both gnome and galago want allow for -1
+}
+
+impl Into<notify_rust::Notification> for &Notification {
+    fn into(self) -> notify_rust::Notification {
+        let mut notification = notify_rust::Notification::new();
+
+        if let Some(app_name) = &self.appname {
+            notification.appname(&app_name);
+        }
+
+        if let Some(summary) = &self.summary {
+            notification.summary(&summary);
+        }
+
+        if let Some(icon) = &self.icon {
+            notification.icon(&icon);
+        }
+
+        notification.finalize()
+    }
+}
+
+impl Notification {
+    fn show(&self) {
+        let notification: notify_rust::Notification = self.into();
+        notification.show().unwrap();
+    }
 }
 
 pub fn show(_interface: &mut dyn Interface, data: &[u8], _zero_copy: Option<ZeroCopyBuf>) -> Op {
@@ -44,10 +70,13 @@ pub fn show(_interface: &mut dyn Interface, data: &[u8], _zero_copy: Option<Zero
     println!("-- send --\n{:#?}", content);
 
     match serde_json::from_slice::<'_, Notification>(data) {
-     Ok(notification) => println!("-- notification--\n{:#?}", notification),
-     Err(e) => eprintln!("{}", e),
-
+        Ok(notification) => {
+            println!("-- notification--\n{:#?}", notification);
+            notification.show();
+        }
+        Err(e) => eprintln!("{}", e),
     }
+
     let result = b"test";
     let result_box: Buf = Box::new(*result);
     Op::Sync(result_box)
